9f3d885371ead94c9bf045e284e4b88370b4ac93
hausidau <hausidau>
2026-03-25 19:42:22 +0700

    css home

---
+++ BanHang/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class IndexModel : PageModel
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public IndexModel(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public string Username { get; set; }
+
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    public class InputModel
+    {
+        public string PhoneNumber { get; set; }
+    }
+
+    public async Task OnGetAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        Username = user.UserName;
+
+        Input = new InputModel
+        {
+            PhoneNumber = user.PhoneNumber
+        };
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        user.PhoneNumber = Input.PhoneNumber;
+
+        await _userManager.UpdateAsync(user);
+
+        TempData["StatusMessage"] = "Cập nhật thành công!";
+        return RedirectToPage();
+    }
+}
